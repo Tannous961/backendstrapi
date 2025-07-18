ee6a86b8a87cb71259c1643281cbb1e67de03eb8
Tannous961 <tannousmekary@gmail.com>
2025-07-18 16:56:37 +0200

    Update plugins.ts

--- cx-firstbackend/config/plugins.ts
+++ cx-firstbackend/config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: env('UPLOAD_PROVIDER', 'local'),
+      providerOptions: {
+        // ...options spécifiques au provider cloud
+      },
+    },
+  },
+});
